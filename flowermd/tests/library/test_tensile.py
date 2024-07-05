499f359ee1e01301cd1de7da71c85464fd59e435
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2024-07-05 17:29:12 -0600

    add tests

--- flowermd/tests/library/test_tensile.py
+++ flowermd/tests/library/test_tensile.py
@@ -29,3 +29,35 @@ class TestTensileSimulation(BaseTest):
             strain=0.05, kT=2.0, n_steps=1e3, period=10, tau_kt=0.001
         )
         assert np.allclose(tensile_sim.strain, 0.05, 1e-4)
+
+    def test_log_data(self):
+        pps = PPS(lengths=6, num_mols=32)
+        system = Lattice(
+            molecules=[pps],
+            x=1.2,
+            y=1.2,
+            n=4,
+        )
+        system.apply_forcefield(
+            r_cut=2.5, force_field=[OPLS_AA_PPS()], auto_scale=True
+        )
+
+        tensile_sim = Tensile(
+            initial_state=system.hoomd_snapshot,
+            forcefield=system.hoomd_forcefield,
+            tensile_axis=(1, 0, 0),
+            log_write_freq=1e2,
+            gsd_write_freq=1e6,
+        )
+        tensile_sim.run_tensile(
+            strain=0.05, kT=2.0, n_steps=1e3, period=10, tau_kt=0.001
+        )
+        assert len(tensile_sim._strain_logs) == 1
+        assert len(tensile_sim._stress_logs) == 1
+        assert len(tensile_sim._strain_logs[0][1:]) == 10
+        tensile_sim.run_tensile(
+            strain=0.05, kT=2.0, n_steps=5e2, period=10, tau_kt=0.001
+        )
+        assert len(tensile_sim._strain_logs) == 2
+        assert len(tensile_sim._stress_logs) == 2
+        assert len(tensile_sim._strain_logs[2][1:]) == 5
