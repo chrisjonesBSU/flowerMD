7afed6ad898773fd7c0b18831e2806f2b5ae666d
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2024-07-05 18:15:46 -0600

    test logged strain data

--- flowermd/tests/library/test_tensile.py
+++ flowermd/tests/library/test_tensile.py
@@ -46,18 +46,17 @@ class TestTensileSimulation(BaseTest):
             initial_state=system.hoomd_snapshot,
             forcefield=system.hoomd_forcefield,
             tensile_axis=(1, 0, 0),
-            log_write_freq=1e2,
+            log_write_freq=10,
             gsd_write_freq=1e6,
         )
         tensile_sim.run_tensile(
-            strain=0.05, kT=2.0, n_steps=1e3, period=10, tau_kt=0.001
+            strain=0.05, kT=2.0, n_steps=1e3, period=100, tau_kt=0.001
         )
         assert len(tensile_sim._strain_logs) == 1
         assert len(tensile_sim._stress_logs) == 1
-        assert len(tensile_sim._strain_logs[0][1:]) == 10
-        tensile_sim.run_tensile(
-            strain=0.05, kT=2.0, n_steps=5e2, period=10, tau_kt=0.001
-        )
-        assert len(tensile_sim._strain_logs) == 2
-        assert len(tensile_sim._stress_logs) == 2
-        assert len(tensile_sim._strain_logs[2][1:]) == 5
+        assert len(tensile_sim._strain_logs[0][1:]) == 100
+        strain_data = tensile_sim.strain_data
+        unique_strains = np.unique(strain_data)
+        dstrain = unique_strains[-1] - unique_strains[-2]
+        assert np.allclose(strain_data[0], 0.0, atol=1e-5)
+        assert np.allclose(strain_data[-1], 0.05 - dstrain, atol=1e-5)
